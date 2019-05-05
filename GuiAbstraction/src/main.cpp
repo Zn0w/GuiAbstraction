ac0c84d0348bc90efb884fdb266f1c6b898d7a3e
Daniil Blagiy <blagiy.daniil@gmail.com>
2019-05-05 16:46:32 +0300

    added a basic element visualization (for testing)

--- GuiAbstraction/src/main.cpp
+++ GuiAbstraction/src/main.cpp
@@ -6,8 +6,7 @@
 
 bool running;
 
-//HMENU menu;		// A window's main menu (on the top of a screen)
-
+#if 0
 int main()
 {
 	zn::Window test_window(500, 500, 400, 400, "Test Window");
@@ -48,33 +47,9 @@ int main()
 
 	system("pause");
 }
-#if 0
-enum MenuItem
-{
-	MENU_FILE = 1,	// New, Open, Close, Save, Save As
-	MENU_EDIT,		// Cut, Copy, Paste, Delete, Undo
-	MENU_VIEW,		// Selection of menus
-	MENU_DATABASE,	// Close, New, Open, Exit
-	MENU_SETTINGS,	// Opens the settings window
-	MENU_TOOLS,		// Opens tools window, or shows the selection of tools
-	MENU_SCRIPTING,	// New Script, Openg Script, Run Script File
-	MENU_HELP		// Opens a help window
-};
+#endif;
 
-
-void initWindow(HWND handle_window)
-{
-	HMENU menu = CreateMenu();
-	AppendMenu(menu, MF_STRING, MENU_FILE, "File");
-	AppendMenu(menu, MF_STRING, MENU_EDIT, "Edit");
-	AppendMenu(menu, MF_STRING, MENU_VIEW, "View");
-	AppendMenu(menu, MF_STRING, MENU_DATABASE, "Database");
-	AppendMenu(menu, MF_STRING, MENU_SETTINGS, "Settings");
-	AppendMenu(menu, MF_STRING, MENU_TOOLS, "Tools");
-	AppendMenu(menu, MF_STRING, MENU_SCRIPTING, "Scripting");
-	AppendMenu(menu, MF_STRING, MENU_HELP, "Help");
-	SetMenu(handle_window, menu);
-}
+std::vector<zn::Element>* test_elements;
 
 
 LRESULT CALLBACK WindowProc(
@@ -88,54 +63,11 @@ LRESULT CALLBACK WindowProc(
 	{
 		case WM_CREATE :
 		{
-			initWindow(handle_window);
 			OutputDebugString("WM_CREATE\n");
 		}break;
 
 		case WM_COMMAND:
 		{
-			switch (wParam)
-			{
-				case (WPARAM) MENU_FILE :
-				{
-					MessageBeep(MB_OK);
-				}break;
-
-				case MENU_EDIT:
-				{
-
-				}break;
-
-				case MENU_VIEW:
-				{
-
-				}break;
-
-				case MENU_DATABASE:
-				{
-
-				}break;
-
-				case MENU_SETTINGS:
-				{
-
-				}break;
-
-				case MENU_TOOLS:
-				{
-
-				}break;
-
-				case MENU_SCRIPTING:
-				{
-
-				}break;
-
-				case MENU_HELP:
-				{
-
-				}break;
-			}
 			OutputDebugString("WM_COMMAND\n");
 		}break;
 		
@@ -157,6 +89,24 @@ LRESULT CALLBACK WindowProc(
 			OutputDebugString("WM_ACTIVATEAPP\n");
 		}break;
 
+		case WM_PAINT:
+		{
+			// TODO : handle event with event system and change window_data or use windows's default handling (with paint struct)
+			PAINTSTRUCT paint;
+			HDC device_context = BeginPaint(handle_window, &paint);
+
+			for (int i = 0; i < test_elements->size(); i++)
+			{
+				Rectangle(device_context,
+					test_elements->at(i).getX(),
+					test_elements->at(i).getY(),
+					test_elements->at(i).getX() + test_elements->at(i).getWidth(),
+					test_elements->at(i).getY() + test_elements->at(i).getHeight());
+			}
+
+			EndPaint(handle_window, &paint);
+		}break;
+
 		default:
 		{
 			return DefWindowProcA(handle_window, message, wParam, lParam);
@@ -166,12 +116,49 @@ LRESULT CALLBACK WindowProc(
 
 int WINAPI WinMain(HINSTANCE instance, HINSTANCE previous_instance, LPSTR cmd, int cmd_show)
 {
-	zn::Window window_data(250, 250, 250, 250, "Test window Title");
+	//zn::Window window_data(250, 250, 250, 250, "Test window Title");
+	zn::Window window_data(500, 500, 400, 400, "Test Window");
+	zn::Frame test_frame(new zn::SequenceLayout(zn::X_AXIS, 400, 400, 5, 5));
+	window_data.setFrame(&test_frame);
+
+	test_frame.addElement(zn::Element("Button1", 20, 15));
+	test_frame.addElement(zn::Element("Button2", 20, 15));
+	test_frame.addElement(zn::Element("Button3", 20, 15));
+	test_frame.addElement(zn::Element("Button4", 20, 15));
+	test_frame.addElement(zn::Element("Button5", 20, 15));
+
+	for (int i = 0; i < test_frame.getElements()->size(); i++)
+	{
+		printf(
+			"Element %s (%d, %d, %d, %d)\n",
+			test_frame.getElement(i)->getContents(),
+			test_frame.getElement(i)->getX(),
+			test_frame.getElement(i)->getY(),
+			test_frame.getElement(i)->getWidth(),
+			test_frame.getElement(i)->getHeight()
+		);
+	}
+
+	test_frame.allignElements();
+
+	for (int i = 0; i < test_frame.getElements()->size(); i++)
+	{
+		printf(
+			"Element %s (%d, %d, %d, %d)\n",
+			test_frame.getElement(i)->getContents(),
+			test_frame.getElement(i)->getX(),
+			test_frame.getElement(i)->getY(),
+			test_frame.getElement(i)->getWidth(),
+			test_frame.getElement(i)->getHeight()
+		);
+	}
+
+	test_elements = test_frame.getElements();
 
 	// 'A' in the end of WNDCLASS means to handle strings in window as ansi code
 	WNDCLASSA window_class = {};	// initializes every element of windows class  to 0
 
-	window_class.hbrBackground = (HBRUSH) COLOR_WINDOW;		// Set a default window color
+	window_class.hbrBackground = (HBRUSH)COLOR_WINDOW;		// Set a default window color
 	window_class.hCursor = LoadCursor(NULL, IDC_ARROW);	// Enable a cursor in window
 	window_class.lpfnWndProc = WindowProc;
 	window_class.hInstance = instance;
@@ -191,7 +178,7 @@ int WINAPI WinMain(HINSTANCE instance, HINSTANCE previous_instance, LPSTR cmd, i
 			NULL,
 			instance,
 			NULL
-			);
+		);
 
 		if (window_handle)
 		{
@@ -213,6 +200,6 @@ int WINAPI WinMain(HINSTANCE instance, HINSTANCE previous_instance, LPSTR cmd, i
 		}
 	}
 
-	
+
 	return 0;
-#endif
+}
