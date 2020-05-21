6b7c42a4042f268302b44a04945b499113ae8370
Daniil Blagiy <blagiy.daniil@gmail.com>
2020-05-21 15:56:06 +0300

    added HorizontalContainer

--- GuiAbstraction/src/zga.h
+++ GuiAbstraction/src/zga.h
@@ -175,6 +175,109 @@ public:
 	}
 };
 
+class HorizontalContainer : public Container
+{
+protected:
+	HorizontalAlignMode hor_align_mode;
+	VerticalAlignMode vert_align_mode;
+	int margin = 25;
+
+
+protected:
+	void align()
+	{
+		int x = 0;
+		if (vert_align_mode == TOP)
+		{
+			for (Component* component : components)
+			{
+				Position new_position = component->getPosition();
+				new_position.x = position.x + x;
+				new_position.y = position.y;
+				component->setPosition(new_position);
+				x += new_position.width;
+				x += margin;
+			}
+		}
+		else if (vert_align_mode == BOTTOM)
+		{
+			for (Component* component : components)
+			{
+				Position new_position = component->getPosition();
+				new_position.x = position.x + x;
+				new_position.y = position.y + position.height - new_position.height;
+				component->setPosition(new_position);
+				x += new_position.width;
+				x += margin;
+			}
+		}
+		else if (vert_align_mode == VERTICAL_CENTER)
+		{
+			for (Component* component : components)
+			{
+				Position new_position = component->getPosition();
+				new_position.x = position.x + x;
+				new_position.y = position.y + position.height / 2 - new_position.height / 2;
+				component->setPosition(new_position);
+				x += new_position.width;
+				x += margin;
+			}
+		}
+		
+		if (hor_align_mode == LEFT)
+		{
+			// no changes needed
+		}
+		else if (hor_align_mode == RIGHT)
+		{
+			Position last_element_position = components.back()->getPosition();
+			int space_left_until_right = (position.x + position.width) - (last_element_position.x + last_element_position.width);
+			if (space_left_until_right)
+			{
+				for (Component* component : components)
+				{
+					Position new_position = component->getPosition();
+					new_position.x += space_left_until_right;
+					component->setPosition(new_position);
+				}
+			}
+		}
+		else if (hor_align_mode == HORIZONTAL_CENTER)
+		{
+			Position last_element_position = components.back()->getPosition();
+			int space_left_until_right = (position.x + position.width) - (last_element_position.x + last_element_position.width);
+			int offset = space_left_until_right / 2;
+			if (space_left_until_right)
+			{
+				for (Component* component : components)
+				{
+					Position new_position = component->getPosition();
+					new_position.x += offset;
+					component->setPosition(new_position);
+				}
+			}
+		}
+	}
+
+public:
+	HorizontalContainer(HorizontalAlignMode s_hor_align_mode, VerticalAlignMode s_vert_align_mode, Position s_position)
+		: hor_align_mode(s_hor_align_mode), vert_align_mode(s_vert_align_mode)
+	{
+		position = s_position;
+	}
+
+	void setMargin(int value) { margin = value; }
+	int getMargin() { return margin; }
+
+	void render(sf::RenderWindow* window)
+	{
+		align();
+		for (Component* component : components)
+			if (component->isVisible())
+				component->render(window);
+	}
+};
+
 // Containers implementations ...
 
 class Label : public Component
