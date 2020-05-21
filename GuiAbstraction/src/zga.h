ce172c07d62f3e047f225f2731b3e92917e05c4d
Daniil Blagiy <blagiy.daniil@gmail.com>
2020-05-21 16:28:33 +0300

    added functions to work with components collection in Container class

--- GuiAbstraction/src/zga.h
+++ GuiAbstraction/src/zga.h
@@ -41,11 +41,35 @@ protected:
 protected:
 	virtual void align() = 0;
 public:
-	void add(Component* component) { components.push_back(component); }
-	//void remove(Component cpmponent) { components }
+	void add(Component* component)
+	{
+		components.push_back(component);
+	}
 
-	// remove??
-	//std::vector<Component*> getComponents() { return components; }
+	int get_index(Component* component)
+	{
+		std::vector<Component*>::iterator it = std::find(components.begin(), components.end(), component);
+		int index = std::distance(components.begin(), it);
+		return index;
+	}
+
+	void remove(Component* component)
+	{
+		int index = get_index(component);
+		components.erase(components.begin() + index);
+	}
+
+	void remove(int index)
+	{
+		components.erase(components.begin() + index);
+	}
+
+	void setComponents(std::vector<Component*> s_components)
+	{
+		components = s_components;
+	}
+
+	std::vector<Component*> getComponents() { return components; }
 };
 
 class AbsoluteContainer : public Container
